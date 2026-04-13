ca581483f9ead0a39e4cf77faa9f194539f107a7
Erik James Robles <ejames@ejames.local>
2026-04-13 09:14:10 -0600

    remove node_modules, dist, and env files from repo and add to gitignore

--- dist/assets/loader-circle-BJWojs1l.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-DxV1YwVn.js";var t=e(`arrow-right`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`m12 5 7 7-7 7`,key:`xquz4c`}]]),n=e(`circle-check`,[[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}],[`path`,{d:`m9 12 2 2 4-4`,key:`dzmm74`}]]),r=e(`loader-circle`,[[`path`,{d:`M21 12a9 9 0 1 1-6.219-8.56`,key:`13zald`}]]);export{n,t as r,r as t};
